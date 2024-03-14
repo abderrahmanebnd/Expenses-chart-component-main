d0126fe6df93fa8bae5bb67a7b92a0c325cd75a1
Abderrahmane bnd <abdoubendaia7@gmail.com>
2024-03-14 13:57:48 +0100

    Challenge completed !

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+const data = [
+  {
+    day: "mon",
+    amount: 17.45,
+  },
+  {
+    day: "tue",
+    amount: 34.91,
+  },
+  {
+    day: "wed",
+    amount: 52.36,
+  },
+  {
+    day: "thu",
+    amount: 31.07,
+  },
+  {
+    day: "fri",
+    amount: 23.39,
+  },
+  {
+    day: "sat",
+    amount: 43.28,
+  },
+  {
+    day: "sun",
+    amount: 25.48,
+  },
+];
 
-function App() {
+const maxAmount = Math.max(...data.map((item) => item.amount));
+const sum = data.reduce((total, curItem) => total + curItem.amount, 0);
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Main />
     </div>
   );
 }
 
-export default App;
+function Header() {
+  return (
+    <header className="header">
+      <div>
+        <span>My balance</span>
+        <span>${sum}</span>
+      </div>
+      <div className="logo-cont">
+        <img src="/images/logo.svg" alt="logo" className="logo" />
+      </div>
+    </header>
+  );
+}
+
+function Main() {
+  return (
+    <main className="main">
+      <Stats />
+      <Conclusion />
+    </main>
+  );
+}
+
+function Stats() {
+  return (
+    <section>
+      <div className="header">Spending - Last 7 days</div>
+      <Chart />
+    </section>
+  );
+}
+
+function Chart() {
+  return (
+    <div className="chart">
+      {data.map((item) => (
+        <Bar
+          amount={item.amount}
+          day={item.day}
+          key={item.day}
+          maxAmount={maxAmount}
+        />
+      ))}
+    </div>
+  );
+}
+
+function Bar({ amount, day }) {
+  const today = new Date().toDateString().slice(0, 3).toLowerCase();
+  const bgColor = today === day ? "hsl(186, 34%, 60%)" : "";
+  const maxHeight = 100;
+  const barHeight = (amount / maxAmount) * maxHeight;
+  return (
+    <div className="bar-cont">
+      <div
+        className="bar"
+        style={{
+          height: `${barHeight}%`,
+          backgroundColor: bgColor,
+        }}
+      >
+        <p className="amount">${amount}</p>
+      </div>
+      <p>{day}</p>
+    </div>
+  );
+}
+function Conclusion() {
+  return (
+    <div className="conclusion">
+      <div>
+        <span>Total this month </span>
+        <span>$546.33</span>
+      </div>
+      <div>
+        <span>+2.4% </span>
+        <span>from last month</span>
+      </div>
+    </div>
+  );
+}
